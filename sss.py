94a497c0845ea3bea2fd68c3ef704f6a0370460d
sunsaina <164596213@qq.com>
2017-09-13 08:47:10 +0800

    增加了一些功能

--- sss.py
+++ sss.py
@@ -1,3 +1,4 @@
+import time;
 for i in range(1,10):
     for j in range(1,10):
         if(j<=i):
@@ -7,4 +8,7 @@ for i in range(1,10):
             break;
 
 
-
+print ('hello');print ('runoob');
+list = ['runoob', 786, 2.23, 'john', 70.2]
+print (list[1:3])
+print (time.asctime( time.localtime(time.time()) ))
